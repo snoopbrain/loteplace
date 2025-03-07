813fd11017cb79c30f95116cddf9d391b495bfb1
SrRobls <joroblesr@unal.edu.co>
2025-03-07 14:59:41 -0500

    Feat: add admin properties page to manage user properties

--- src/services/authenticationService.js
+++ src/services/authenticationService.js
@@ -1,6 +1,7 @@
 // authenticationService.js
 const APIURL = 'http://146.190.143.234:8080/api/auth';
 
+
 export const login = async (email, password) => {
   const payload = { email, password };
 
@@ -53,3 +54,37 @@ export const signup = async (name, email, password, municipality, contact) => {
   }
 
 }; 
+
+export const validSession = async (token, email) => {
+  // Construimos el objeto con el token y el email
+  const payload = { token, email };
+
+  try {
+    const response = await fetch(`${APIURL}/verificar_token`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // OJO: No es estándar. fetch ignora el body en GET en la mayoría de los casos.
+      body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      // Si el servidor responde con 500 y un mensaje específico de JWT
+      if (response.status === 500 && errorData.details?.includes("JWT signature does not match")) {
+        console.error("JWT inválido, cerrando sesión...");
+        return { isValid: false, error: 'INVALID_JWT' };
+      }
+      // Si el token expiró u otro error
+      return { isValid: false };
+    }
+
+    // Si la sesión es válida
+    return { isValid: true };
+
+  } catch (error) {
+    console.error('Error en validSession:', error);
+    return { isValid: false, error: 'UNKNOWN_ERROR' };
+  }
+};
